617392947c86e02fb673e1f20e64daf08224d5e1
noonanri <noonanri@5DJQJG2.itserv.scss.tcd.ie>
2017-12-12 15:40:44 +0000

    -interrogation of github api

---
+++ src/Int.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class Int {
+
+
+	public static void main(String[] args) throws Exception{
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("What is your GitHub Username?");
+		String username = scanner.next();
+		System.out.println("What repo do you want?");
+		String repo = scanner.next();
+		HttpURLConnection httpcon = (HttpURLConnection) new URL("https://api.github.com/repos/" 
+			+ username + "/" + repo).openConnection();														
+		scanner.close();
+		
+		httpcon.addRequestProperty("User-Agent", "Chrome");
+		BufferedReader in = new BufferedReader(new InputStreamReader(httpcon.getInputStream()));
+		
+		StringBuilder responseSB = new StringBuilder();
+		String line;
+		while (( line = in.readLine()) != null) {
+			responseSB.append("\n" + line);
+			System.out.print(line);
+		}
+		in.close();	
+}	
+}
