06830c30e639ab066f6bb312c1596c2d72696c72
noonanri <noonanri@5DJQJG2.itserv.scss.tcd.ie>
2017-12-12 16:55:21 +0000

    Alternative method of displaying data on github user

---
+++ Github/GithubIntAlt.java
@@ -0,0 +1,34 @@
+package Github;
+
+import java.util.Scanner;
+import java.io.*;
+import org.eclipse.egit.github.core.User;
+import org.eclipse.egit.github.core.client.GitHubClient;
+import org.eclipse.egit.github.core.service.UserService;
+
+public class GithubIntAlt {
+	public static void main(String[] args) throws IOException {
+		System.out.println("Enter your username:\n");
+		Scanner input = new Scanner (System.in);
+		String username = input.next();
+		System.out.println("Enter your password:\n");
+		String password = input.next();
+		input.close();
+		User user = FindUser(username, password);
+		System.out.println("Information on this user:\nLogin: "
+							+ user.getLogin() + "\nName: "
+							+ user.getName() + "\nEmail: "
+							+ user.getEmail() + "\nFollowers: "
+							+ user.getFollowers() + "\nFollowing: "
+							+ user.getFollowing() + "\nPublic Repos: "
+							+ user.getPublicRepos() + "\nPrivate Repos: "
+							+ user.getTotalPrivateRepos());
+		}
+	
+	private static User FindUser(String username, String password) throws IOException {
+		GitHubClient client = new GitHubClient();
+		client.setCredentials(username, password);
+		UserService userService = new UserService(client);
+		return userService.getUser();
+	}
+}
